03d31475da50ee1b58842d3d6a86b72004b65da9
Student <student@w705-14.7.kpm.mipt>
2014-02-17 19:43:02 +0400

    Add cw folder and my_printf.s

---
+++ cw/my_printf.s
@@ -0,0 +1,37 @@
+	.data
+msg_string:
+	.string "Call 1\n"
+msg_string_2:
+	.string "Call 2\n"
+	.text
+	.globl main
+my_printf:
+	pushl %ebp
+        movl %esp, %ebp
+			
+	pushl $msg_string_2
+	pushl $msg_string
+	
+	call printf
+	
+	addl $4, %esp
+
+	call printf
+
+	addl $4, %esp	
+
+	movl %ebp, %esp
+        popl %ebp
+
+        ret
+
+main:
+	pushl %ebp
+        movl %esp, %ebp
+
+	call my_printf
+	
+        movl %ebp, %esp
+        popl %ebp
+
+	ret
