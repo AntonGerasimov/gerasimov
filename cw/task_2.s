150be91e30e4701caf04604d1e85599840739670
Student <student@w705-14.7.kpm.mipt>
2014-02-24 19:19:11 +0400

    Add task_2.s

---
+++ cw/task_2.s
@@ -0,0 +1,22 @@
+	.data
+string_label:
+	.string "Hello"
+	.text
+	.globl main
+format_string:
+        .string "%s %d\n"
+        .text
+        .globl  main
+main: 
+	pushl %ebp
+	movl %esp, %ebp
+
+	pushl $123
+	pushl $string_label
+	pushl $format_string
+	call printf
+	addl $8, %esp
+
+	movl %ebp, %esp
+	popl %ebp
+	ret
