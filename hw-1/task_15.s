b43ddee7b00932abb9e68b0fa4590a6fc4967b74
AntonGerasimov <antoshkasg@gmail.com>
2014-03-25 20:06:25 +0400

    Add task_15 - task_21 + task_24 with tests

--- hw-1/task_15.s
+++ hw-1/task_15.s
@@ -1,37 +1,52 @@
-        .data
-int:
-        .space 4
-in:
-        .string "%d"
-yes:
-        .string "yes\n"
-no:
-        .string "no\n"
-        .text
-        .globl main
+#Программа получает на вход 2 целых числа и выводит "Yes", если первое число больше или равно, чем второе, и "No" в обратном случае. 
+#Вывод производится без кавычек.
+	.data
+yes_string:
+	.string "Yes\n"
+no_string:
+	.string "No\n"
+int_string:
+	.string "%d %d"
+int_space_a:
+	.space 4
+int_space_b:
+	.space 4
+	.text
+	.globl main
 main:
-        pushl %ebp
-        movl %esp, %ebp
+	pushl %ebp #Prolog
+	movl %esp, %ebp
 
-        pushl $int
-        pushl $in
-        call scanf
-        addl $8, %esp
+	pushl $int_space_b #Scan
+	pushl $int_space_a
+	pushl $int_string
+	call scanf
+	addl $8, %esp
 
-        movl int, %eax
-        cmp $1,%eax
-        je one
-        pushl $no
-        call printf
-        addl $4, %esp
-        jmp finish
-one:
-        pushl $yes
-        call printf
-        addl $4, %esp
-finish:
-        movl %ebp, %esp
-        popl %ebp
-	movl $0, %eax
-        ret
+	pushl %ebx #Save ebx and eax
+	pushl %eax
 
+	movl int_space_a, %eax
+	movl int_space_b, %ebx
+	
+	cmp %ebx, %eax
+	jge more #If the first is more then the second or equal go to more
+	pushl $no_string #Print if second is more then the first
+	call printf
+	addl $4, %esp
+	jmp next
+
+more:
+	pushl $yes_string #Print
+	call printf
+	addl $4, %esp
+
+next: #Continue program	
+	popl %eax
+	popl %ebx
+
+	movl %ebp, %esp #Epilog
+	popl %ebp
+
+	movl $0, %eax #return 0
+	ret
---
+++ hw-1/task_17.s
@@ -0,0 +1,47 @@
+#На вход подается два целых числа. Второе из них положительное. Программа возводит первое число в степень второго.
+	.data
+out_string:
+	.string "%d\n"
+in_string:
+	.string "%d %d"
+int_space_a:
+	.space 4
+counter:
+	.space 4
+	.text
+	.globl main
+main:
+	pushl %ebp #Prolog
+	movl %esp, %ebp
+
+	pushl $counter #Scan
+	pushl $int_space_a
+	pushl $in_string
+	call scanf
+
+
+	movl int_space_a, %eax
+	movl %eax, 4(%esp)
+	movl $1, 8(%esp) #for (i=1;
+	
+umn:	#Умножение на первоначальное число
+	movl 4(%esp), %ebx
+	imull %ebx
+	addl $1, 8(%esp) #i++
+proverka:
+	movl 8(%esp), %ebx
+	cmpl %ebx, counter #i<n
+	jne umn
+
+	pushl %eax
+	pushl $out_string
+	call printf
+	addl $16, %esp	
+	jmp next
+
+next: #Continue program	
+	movl %ebp, %esp #Epilog
+	popl %ebp
+	
+	movl $0, %eax #return 0
+	ret
