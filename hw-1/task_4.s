58d3c6152a4f50fed8e97750e582d362b78ab0ee
AntonGerasimov <antoshkasg@gmail.com>
2014-03-27 00:41:30 +0400

    Add not added before files because of mistake

---
+++ hw-1/task_4.s
@@ -0,0 +1,57 @@
+#На вход программы подается число в шестнадцатеричной системе счисления. Программа выводит его запись в двоичном представлении.
+	.data
+in:
+	.string "%x" #%x is for hexadecimal numbers 
+int:
+	.space 4
+out:
+	.string "\n"
+	.text
+	.globl main
+main:
+	pushl	%ebp	#Prolog
+	movl	%esp,	%ebp
+
+	pushl	$int	#Scan
+	pushl	$in
+	call	scanf
+	addl	$8,	%esp
+
+	movl	int,	%eax	#our number is in eax now
+	movl	$32,	%ecx	#set counter to 32
+	movl	$0,	%edx	#edx становится равно 1, когда в числе появляется превая единица. Так отбрасываются незначащие нули.
+
+shift:
+	shll	$1,	%eax
+	movl	$0,	%ebx
+	jnc	print	#проверяем carry flag
+	movl	$1,	%ebx
+	movl	$1,	%edx 
+print:
+	cmpl	$1,	%edx
+	jne	finish
+
+	pushl	%edx	#сохраняем значения edx, ecx, eax перед печатью, кладя их на стек
+	pushl	%ecx
+	pushl	%eax
+	
+	pushl	%ebx	#печать очередной цифры числа
+	pushl	$in
+	call	printf
+	addl	$8,	%esp
+
+	popl	%eax	#забираем значения edx, ecx, eax со стека
+	popl	%ecx
+	popl	%edx
+finish:
+	loop	shift
+
+	pushl	$out
+	call	printf
+	addl	$4, %esp
+
+	movl	%ebp,	%esp	#Epilog
+	popl	%ebp
+
+	movl	$0,	%eax	#return 0
+ret
