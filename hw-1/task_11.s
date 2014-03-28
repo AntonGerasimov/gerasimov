b7abfa35658e5db7851bc91df5e3f5ae58a194c1
AntonGerasimov <antoshkasg@gmail.com>
2014-03-28 23:37:31 +0400

    Add task_11

---
+++ hw-1/task_11.s
@@ -0,0 +1,40 @@
+	.data
+int:
+	.space 8
+space:
+	.space 8
+address:
+	.int 	0x00110012
+in_int:
+	.string "%d"
+out_int:
+	.string "%d\n"
+	.text
+	.globl main
+main:
+	pushl	%ebp #Prolog
+	movl	%esp,	%ebp
+
+	pushl 	$int #Scan number
+	pushl	$in_int
+	call	scanf
+	addl 	$8,	%esp
+
+	movl	$space,	%ebx #mov address of space to ebx
+	movl	int,	%eax #mov our scanned value to space
+	movl	%eax,	space
+
+	movl	$23,	%eax 	#Непосредственная адресация
+	movl	(address),	%eax	#Прямая адресация (обычная)	
+	movl	(%ebx),	%eax	#Косвенная адресация
+
+	pushl	%eax #напечатать значение, которое находится по адресу, лежащему в ebx
+	pushl	$out_int
+	call	printf
+	addl	$8,	%esp
+
+	movl	%ebp,	%esp #Epilog
+	popl	%ebp
+	movl	$0,	%eax #return 0
+	ret
+	
