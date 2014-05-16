01929a6f2941c35b3446b6e857be4299935c34ab
AntonGerasimov <antoshkasg@gmail.com>
2014-05-16 21:58:15 +0400

    Add task_3.s

--- hw-2/task_3.s
+++ hw-2/task_3.s
@@ -1,17 +1,17 @@
-#Программа принимает на вход одномерный массив из трех целых чисел.
+#Программа принимает на вход одномерный массив из трех вещественных чисел формата double
 #На выход подается их сумма.
 	.data
 	.globl a
 a:
-	.space 12
+	.space 24
 b:
-	.space 4
+	.space 8
 in:
-	.string	"%f"
+	.string	"%lf"
 out:
-	.string "%f\n"
+	.string "%lf\n"
 summ:
-	.space 4
+	.space 8
 count:
 	.space 4
 	.text
@@ -20,20 +20,12 @@ main:
 	pushl	%ebp 	#Prolog
 	movl	%esp, 	%ebp
 
-//	pushl	$b
-//	pushl	$in
-//	call	scanf
-//	addl	$8,	%esp
+	movl 	$0,	%eax 	#Обнуляем счетчик i
+	movl 	%eax, 	count
 
-//	fl	$b
-
-//	movl 	$0,	%eax 	#Обнуляем счетчик i
-//	movl 	%eax, 	count
-
-//	jmp	next_1
 read:
 	movl	count, 	%eax
-	sall	$2, 	%eax
+	sall	$3, 	%eax
 	addl	$a, 	%eax
 	pushl	%eax
 	pushl	$in
@@ -47,46 +39,40 @@ next_1:
 	cmpl	$2, 	%eax 	#for (i=0, i<=2;i++)
 	jle	read
 
+	finit
+	movl	$a,	%eax
+	fldl	(%eax)	#load a[0] to stack
+        movl    $1,     %eax    #i=0
+        movl    %eax,   count
+        jmp     next_2
+summa:
+        movl    count,  %eax    #for (i=1;i<=2;i++)
+        sall    $3,     %eax
+        addl    $a,     %eax
 
-	pushl	a
+	fldl	(%eax)	#load a[i] to stack
+	faddp	#a[0]+a[i] on the top of stack
+
+        movl    count,  %eax	#i++
+        addl    $1,     %eax
+        movl    %eax,   count
+next_2:
+        movl    count,  %eax    #Проверка
+        cmpl    $2,     %eax
+        jle     summa
+
+	pushl	%eax	#Print
+	fstpl	(%esp)	
+	popl	%eax
+	movl	%eax,	summ
+
+	pushl	summ
 	pushl	$out
 	call	printf
 	addl	$8,	%esp
-
-/*	movl 	a, 	%eax 	#summ = a[0]
-	movl 	%eax, 	summ
-
-	movl 	$1, 	%eax 	#i=1
-	movl 	%eax, 	count
-	jmp 	next_2
-summa:
-	movl 	count, 	%eax 	#for (i=1;i<=2;i++)
-	sall 	$2, 	%eax
-	addl 	$a, 	%eax 
-	
-	movl 	(%eax), %edx
-	movl 	summ, 	%eax 	#summ = summ + a[i]
-	addl 	%edx, 	%eax
-	
-	movl 	%eax, 	summ
-
-	movl 	count, 	%eax
-	addl 	$1, 	%eax
-	movl 	%eax, 	count
-next_2:
-	movl 	count, 	%eax 	#Проверка
-	cmpl 	$2, 	%eax
-	jle 	summa
-		
-	movl 	summ, 	%eax 	#Print
-	pushl 	%eax
-	pushl 	$out
-	call 	printf
-	addl 	$8, 	%esp */
 
 	movl 	%ebp, 	%esp 	#Epilog
 	popl 	%ebp
 
 	movl 	$0, 	%eax 	#return 0
 	ret
-
